f3b6f5f79decc02837a5e5a2007ce4c4a20e8927
ChristoLfrg <55689597+ChristoLfrg@users.noreply.github.com>
2020-02-14 15:52:15 +0100

    Add files via upload

--- TD1/main.c
+++ TD1/main.c
@@ -17,7 +17,6 @@ GLfloat angleZ = 0;
 GLfloat angleE = 0; 
 GLfloat angleR = 0; 
 GLfloat angleT = 0; 
-GLfloat triangle = 0; 
 
 void ma_main (){
 	glPushMatrix();
@@ -194,24 +193,104 @@ void ma_main (){
   glPopMatrix();
 }
 
+GLfloat triangle = 0; 
+GLfloat time = 0;
+
 void mon_triangle(){
 	glPushMatrix();
 	{
-			glRotatef(triangle, 0, 1, 0);
+			glRotatef(triangle, 0, 0, 0);
 			triangle = triangle+1;
 			glBegin(GL_TRIANGLES);
 				glVertex3f(0,2,0);
-				glVertex3f(0,-1,0);
 				glVertex3f(-1,-1,0);
+				glVertex3f(1,-1,0);
 			glEnd();
 	}
 	glPopMatrix();
 	
-	time = glutGet(GLUT_ELAPSED_TIME);
+	//time = glutGet(GLUT_ELAPSED_TIME);
 
-	printf=(time);
+	//printf=(time);
 
 }
+
+// 0 : avant, 1 : arriere, 2 : droit, 3 : gauche, 4 : dessus, 5 : dessous
+static int faces[6][4] = {
+	{0, 1, 2, 3}, 		//AVANT
+	{4, 5, 6, 7}, 		//ARRIERE
+	{1, 4, 7, 2}, 		//DROITE
+	{5, 0, 3, 6}, 		//GAUCHE
+	{3, 2, 7, 6}, 		//DESSUS
+	{4, 5, 0, 1}		//DESSOUS
+};
+float sommets[8][3] = {
+	{0, 3, 5}, 
+	{0, 2, 5}, 
+	{0, 2, 4}, 
+	{0, 3, 4}, 
+	{1, 2, 5}, 
+	{1, 3, 5}, 
+	{1, 3, 4}, 
+	{1, 2, 4}
+};
+
+struct cube {
+	int coord[8][3];
+};
+
+struct cube creer_cube(float taille){
+	struct cube c;
+	
+	c.coord[0][0] = -taille;
+	c.coord[0][1] = -taille;
+	c.coord[0][2] = taille;
+	
+	c.coord[1][0] = -taille;
+	c.coord[1][1] = taille;
+	c.coord[1][2] = taille;
+	
+	c.coord[2][0] = taille;
+	c.coord[2][1] = taille;
+	c.coord[2][2] = taille;
+	
+	c.coord[3][0] = taille;
+	c.coord[3][1] = -taille;
+	c.coord[3][2] = taille;
+	
+	c.coord[4][0] = -taille;
+	c.coord[4][1] = -taille;
+	c.coord[4][2] = -taille;
+	
+	c.coord[5][0] = -taille;
+	c.coord[5][1] = taille;
+	c.coord[5][2] = -taille;
+	
+	c.coord[6][0] = taille;
+	c.coord[6][1] = taille;
+	c.coord[6][2] = -taille;
+	
+	c.coord[7][0] = taille;
+	c.coord[7][1] = -taille;
+	c.coord[7][2] = -taille;
+	
+	return c;
+}
+
+void afficher_cube (struct cube c){
+	glBegin(GL_QUADS);
+		for (int i=0; i<6; i++){
+			for (int j=0; j<4; j++){
+				glVertex3f(
+					c.coord[faces[i][j]][0], 
+					c.coord[faces[i][j]][1], 
+					c.coord[faces[i][j]][2]
+					);
+			}
+		}
+	glEnd();
+}
+
 
 GLvoid Modelisation()
 {
@@ -221,8 +300,9 @@ GLvoid Modelisation()
 	
   //ma_main();
   
-  mon_triangle();
-
+  //mon_triangle();
+  
+	afficher_cube(creer_cube(1));
 
   axes();
   glutSwapBuffers();
